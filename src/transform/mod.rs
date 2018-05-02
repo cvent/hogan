f48f2652ca98eaa9444b0870985b0fda92d29fba
Jonathan Morley <jmorley@cvent.com>
2018-05-02 14:11:30 -0400

    Add or helper

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -6,6 +6,8 @@ pub fn handlebars() -> Handlebars {
     let mut handlebars = Handlebars::new();
     handlebars.register_helper("comma-list", Box::new(helpers::comma_delimited_list_helper));
     handlebars.register_helper("equal", Box::new(helpers::equal_helper));
+    handlebars.register_helper("eq", Box::new(helpers::equal_helper));
+    handlebars.register_helper("or", Box::new(helpers::or_helper));
     handlebars.register_helper("yaml-string", Box::new(helpers::yaml_string_helper));
     handlebars.register_helper("url-rm-slash", Box::new(helpers::url_rm_slash_helper));
     handlebars.register_helper("url-add-slash", Box::new(helpers::url_add_slash_helper));
