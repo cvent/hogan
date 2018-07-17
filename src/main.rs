cde0780d7dc4ddf96d20ae13e52ce5bfc2db118f
Morley <JMorley@cvent.com>
2018-07-17 14:52:03 -0400

    Add CLI tests

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,11 @@ extern crate failure;
 extern crate hogan;
 #[macro_use]
 extern crate log;
-#[macro_use]
-extern crate quicli;
 extern crate regex;
 #[macro_use]
 extern crate rouille;
 extern crate shellexpand;
+extern crate stderrlog;
 #[macro_use]
 extern crate structopt;
 
@@ -18,8 +17,8 @@ use failure::Error;
 use hogan::config::ConfigDir;
 use hogan::template::{Template, TemplateDir};
 use regex::{Regex, RegexBuilder};
-use rouille::Response;
 use rouille::input::plain_text_body;
+use rouille::Response;
 use std::fs::File;
 use std::io::Write;
 use std::mem::replace;
@@ -33,8 +32,13 @@ use structopt::StructOpt;
 #[structopt(raw(setting = "structopt::clap::AppSettings::InferSubcommands"))]
 struct App {
     /// Sets the level of verbosity
-    #[structopt(short = "v", long = "verbose", parse(from_occurrences), raw(global = "true"))]
-    verbosity: u64,
+    #[structopt(
+        short = "v",
+        long = "verbose",
+        parse(from_occurrences),
+        raw(global = "true")
+    )]
+    verbosity: usize,
 
     #[structopt(subcommand)]
     cmd: AppCommand,
@@ -49,21 +53,33 @@ enum AppCommand {
         common: AppCommon,
 
         /// Filter environments to render templates for
-        #[structopt(short = "e", long = "environments-filter",
-                    parse(try_from_str = "App::parse_regex"), default_value = ".+",
-                    value_name = "REGEX")]
+        #[structopt(
+            short = "e",
+            long = "environments-filter",
+            parse(try_from_str = "App::parse_regex"),
+            default_value = ".+",
+            value_name = "REGEX"
+        )]
         environments_regex: Regex,
 
         /// Template source (recursive)
-        #[structopt(short = "t", long = "templates", parse(from_os_str), default_value = ".",
-                    value_name = "DIR")]
+        #[structopt(
+            short = "t",
+            long = "templates",
+            parse(from_os_str),
+            default_value = ".",
+            value_name = "DIR"
+        )]
         templates_path: PathBuf,
 
         /// Filter templates to transform
-        #[structopt(short = "f", long = "templates-filter",
-                    parse(try_from_str = "App::parse_regex"),
-                    default_value = "template([-.].+)?\\.(config|ya?ml|properties)",
-                    value_name = "REGEX")]
+        #[structopt(
+            short = "f",
+            long = "templates-filter",
+            parse(try_from_str = "App::parse_regex"),
+            default_value = "template([-.].+)?\\.(config|ya?ml|properties)",
+            value_name = "REGEX"
+        )]
         templates_regex: Regex,
     },
     /// Respond to HTTP requests to transform a template
@@ -73,7 +89,12 @@ enum AppCommand {
         common: AppCommon,
 
         /// Port to serve requests on
-        #[structopt(short = "p", long = "port", default_value = "80", value_name = "PORT")]
+        #[structopt(
+            short = "p",
+            long = "port",
+            default_value = "80",
+            value_name = "PORT"
+        )]
         port: u16,
     },
 }
@@ -86,8 +107,13 @@ struct AppCommon {
     configs_url: String,
 
     /// SSH key to use if configs URL requires authentication
-    #[structopt(short = "k", long = "ssh-key", parse(from_str = "App::parse_path_buf"),
-                default_value = "~/.ssh/id_rsa", value_name = "FILE")]
+    #[structopt(
+        short = "k",
+        long = "ssh-key",
+        parse(from_str = "App::parse_path_buf"),
+        default_value = "~/.ssh/id_rsa",
+        value_name = "FILE"
+    )]
     ssh_key: PathBuf,
 
     /// Throw errors if values do not exist in configs
@@ -112,113 +138,153 @@ impl App {
     }
 }
 
-main!(|args: App, log_level: verbosity| match args.cmd {
-    AppCommand::Transform {
-        templates_path,
-        environments_regex,
-        templates_regex,
-        common,
-    } => {
-        let mut handlebars = hogan::transform::handlebars(common.strict);
+fn main() -> Result<(), Error> {
+    let opt = App::from_args();
 
-        let template_dir = TemplateDir::new(templates_path)?;
-        let mut templates = template_dir.find(templates_regex);
-        println!("Loaded {} template file(s)", templates.len());
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(opt.verbosity + 2)
+        .init()?;
 
-        let config_dir = ConfigDir::new(common.configs_url, &common.ssh_key)?;
-        let environments = config_dir.find(App::config_regex(&environments_regex)?);
-        println!("Loaded {} config file(s)", environments.len());
+    match opt.cmd {
+        AppCommand::Transform {
+            templates_path,
+            environments_regex,
+            templates_regex,
+            common,
+        } => {
+            let mut handlebars = hogan::transform::handlebars(common.strict);
 
-        for environment in environments {
-            println!("Updating templates for {}", environment.environment);
+            let template_dir = TemplateDir::new(templates_path)?;
+            let mut templates = template_dir.find(templates_regex);
+            println!("Loaded {} template file(s)", templates.len());
 
-            for mut template in &mut templates {
-                debug!("Transforming {:?}", template.path);
+            let config_dir = ConfigDir::new(common.configs_url, &common.ssh_key)?;
+            let environments = config_dir.find(App::config_regex(&environments_regex)?);
+            println!("Loaded {} config file(s)", environments.len());
 
-                let rendered = template.render(&handlebars, &environment)?;
-                if let Err(e) = File::create(&rendered.path)?.write_all(&rendered.contents) {
-                    bail!("Error transforming {:?} due to {}", rendered.path, e);
+            for environment in environments {
+                println!("Updating templates for {}", environment.environment);
+
+                for mut template in &mut templates {
+                    debug!("Transforming {:?}", template.path);
+
+                    let rendered = template.render(&handlebars, &environment)?;
+                    if let Err(e) = File::create(&rendered.path)?.write_all(&rendered.contents) {
+                        bail!("Error transforming {:?} due to {}", rendered.path, e);
+                    }
                 }
             }
         }
-    }
-    AppCommand::Server { common, port } => {
-        let mut handlebars = hogan::transform::handlebars(common.strict);
+        AppCommand::Server { common, port } => {
+            let mut handlebars = hogan::transform::handlebars(common.strict);
 
-        let config_dir = ConfigDir::new(common.configs_url, &common.ssh_key)?;
+            let config_dir = ConfigDir::new(common.configs_url, &common.ssh_key)?;
 
-        let mut environments = RwLock::new(config_dir.find(Regex::new(".+")?));
-        let mut config_dir = Mutex::new(config_dir);
+            let mut environments = RwLock::new(config_dir.find(Regex::new(".+")?));
+            let mut config_dir = Mutex::new(config_dir);
 
-        rouille::start_server(("0.0.0.0", port), move |request| {
-            router!(request,
-                (POST) (/refresh) => {
-                    match environments.write() {
-                        Ok(mut environments) => match config_dir.lock() {
-                            Ok(config_dir) => {
-                                replace(environments.deref_mut(), config_dir.refresh().find(Regex::new(".+").unwrap()));
-                                Response::empty_204()
-                            }
+            rouille::start_server(("0.0.0.0", port), move |request| {
+                router!(request,
+                    (POST) (/refresh) => {
+                        match environments.write() {
+                            Ok(mut environments) => match config_dir.lock() {
+                                Ok(config_dir) => {
+                                    replace(environments.deref_mut(), config_dir.refresh().find(Regex::new(".+").unwrap()));
+                                    Response::empty_204()
+                                }
+                                Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                            },
                             Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                        },
-                        Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                    }
-                },
-                // Transform against all configs
-                (POST) (/transform) => {
-                    match environments.read() {
-                        Ok(environments) => {
-                            match request.data() {
-                                Some(data) => match request.get_param("filename") {
-                                    Some(filename) => {
-                                        let mut template = Template {
-                                            path: PathBuf::from(filename),
-                                            read: data,
-                                        };
+                        }
+                    },
+                    // Transform against all configs
+                    (POST) (/transform) => {
+                        match environments.read() {
+                            Ok(environments) => {
+                                match request.data() {
+                                    Some(data) => match request.get_param("filename") {
+                                        Some(filename) => {
+                                            let mut template = Template {
+                                                path: PathBuf::from(filename),
+                                                read: data,
+                                            };
 
-                                        match template.render_to_zip(&handlebars, &environments) {
-                                            Ok(zip) => Response::from_data("application/octet-stream", zip),
-                                            Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                                        }
+                                            match template.render_to_zip(&handlebars, &environments) {
+                                                Ok(zip) => Response::from_data("application/octet-stream", zip),
+                                                Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                                            }
+                                        },
+                                        None => Response::text("Query parameter 'filename' required").with_status_code(400)
                                     },
-                                    None => Response::text("Query parameter 'filename' required").with_status_code(400)
-                                },
-                                None => Response::text("POST body required").with_status_code(400)
-                            }
-                        },
-                        Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                    }
-                },
-                // Transform against a single config
-                (POST) (/transform/{env: String}) => {
-                    match environments.read() {
-                        Ok(environments) => match environments.iter().find(|e| e.environment == env) {
-                            Some(env) => {
-                                let body = try_or_400!(plain_text_body(request));
-                                println!("Transforming {}", body);
-                                match handlebars.render_template(&body, &env.config_data) {
-                                    Ok(rendered) => Response::text(rendered),
-                                    Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                                    None => Response::text("POST body required").with_status_code(400)
                                 }
                             },
-                            None => Response::empty_404()
-                        },
-                        Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                    }
-                },
-                // Return a single config
-                (GET) (/config/{env: String}) => {
-                    match environments.read() {
-                        Ok(environments) => match environments.iter().find(|e| e.environment == env) {
-                            Some(env) => Response::json(env),
-                            None => Response::empty_404()
-                        },
-                        Err(e) => Response::text(format!("{}", e)).with_status_code(500)
-                    }
-                },
-                // default route
-                _ => Response::empty_404()
-            )
-        });
+                            Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                        }
+                    },
+                    // Transform against a single config
+                    (POST) (/transform/{env: String}) => {
+                        match environments.read() {
+                            Ok(environments) => match environments.iter().find(|e| e.environment == env) {
+                                Some(env) => {
+                                    let body = try_or_400!(plain_text_body(request));
+                                    println!("Transforming {}", body);
+                                    match handlebars.render_template(&body, &env.config_data) {
+                                        Ok(rendered) => Response::text(rendered),
+                                        Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                                    }
+                                },
+                                None => Response::empty_404()
+                            },
+                            Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                        }
+                    },
+                    // Return a single config
+                    (GET) (/config/{env: String}) => {
+                        match environments.read() {
+                            Ok(environments) => match environments.iter().find(|e| e.environment == env) {
+                                Some(env) => Response::json(env),
+                                None => Response::empty_404()
+                            },
+                            Err(e) => Response::text(format!("{}", e)).with_status_code(500)
+                        }
+                    },
+                    // default route
+                    _ => Response::empty_404()
+                )
+            });
+        }
     }
-});
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate assert_cli;
+
+    #[test]
+    fn test_transform() {
+        assert_cli::Assert::main_binary()
+            .with_args(&[
+                "transform",
+                "--configs",
+                "tests/fixtures/configs",
+                "--templates",
+                "tests/fixtures/Projects",
+            ])
+            .stdout()
+            .is(r#"Finding Files: "tests/fixtures/Projects"
+regex: /template([-.].+)?\.(config|ya?ml|properties)/
+Loaded 3 template file(s)
+Finding Files: "tests/fixtures/configs"
+regex: /config\..+\.json$/
+Loaded 4 config file(s)
+Updating templates for EMPTY
+Updating templates for ENVTYPE
+Updating templates for TEST
+Updating templates for TEST2"#)
+            .unwrap();
+    }
+}
