b0ca2899715598b729065f694902c430785cb2ab
Josh Comer <jcomer@cvent.com>
2020-03-17 11:49:29 -0300

    ci: only perform release builds and tests

--- ci/script.sh
+++ ci/script.sh
@@ -4,17 +4,17 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
+    #cross build --target $TARGET
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
+    #cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET -- --help
+    #cross run --target $TARGET -- --help
     cross run --target $TARGET --release -- --help
 }
 
